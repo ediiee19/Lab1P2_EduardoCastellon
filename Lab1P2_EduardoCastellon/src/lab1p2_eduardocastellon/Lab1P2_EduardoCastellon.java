6636ba0ee110737795a399e04b1d05be7f46d699
ediiee19 <castelloneduardo2005@gmail.com>
2024-01-19 14:19:20 -0600

    Avacnce 1

---
+++ Lab1P2_EduardoCastellon/src/lab1p2_eduardocastellon/Lab1P2_EduardoCastellon.java
@@ -0,0 +1,118 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab1p2_eduardocastellon;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author caste
+ */
+public class Lab1P2_EduardoCastellon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        Scanner lea = new Scanner(System.in);
+        
+        ArrayList<Usuario> usuarios = new ArrayList();
+
+        boolean seguir = true;
+        while (seguir) {
+            System.out.println("---Registro---");
+            System.out.println("1. Ingresar usuario");
+            System.out.println("2. Listar todo");
+            System.out.println("3. Listar por dominio");
+            System.out.println("presione [4] para salir");
+            int opc = entrada.nextInt();
+
+            switch (opc) {
+                case 4: {
+                    System.out.println("Saliendo del programa...");
+                    seguir = false;
+                    break;
+                }
+                case 1: {
+                    System.out.println("Ingrese su usuario");
+                    System.out.println("Nombre: ");
+                    String nombre = lea.nextLine();
+                    System.out.println("Apellido");
+                    String apellido = lea.nextLine();
+                    String contra = validarContra();
+                    String correo = validarCorreo();
+                    String dom = obtenerDom(correo);
+                    break;
+                }
+                case 2:{
+                    break;
+                }
+            }
+        }
+
+    }
+
+    public static String validarContra() {
+        Scanner lea = new Scanner(System.in);
+
+        System.out.println("Ingrese su contraseña");
+        String contra = lea.nextLine();
+
+        return contra;
+    }
+
+    public static String validarCorreo() {
+        Scanner lea = new Scanner(System.in);
+        String correo = "";
+
+        boolean validarCorreo = true;
+        while (validarCorreo) {
+            System.out.println("Ingrese su correo:");
+            correo = lea.nextLine();
+
+            String dom = obtenerDom(correo);
+            if (dom.equals("gmail.com")) {
+                validarCorreo = false;
+            }
+            if (dom.equals("Outlook.com")) {
+                validarCorreo = false;
+            }
+            if (dom.equals("Yahoo.com")) {
+                validarCorreo = false;
+            }
+            if (dom.equals("iCloud.com")) {
+                validarCorreo = false;
+            }
+            if (dom.equals("ProtonMail.com")) {
+                validarCorreo = false;
+            }
+            if (dom.equals("FastMail.com")) {
+                validarCorreo = false;
+            }else{
+                System.out.println("Ingrese un dominio valido");
+                System.out.println("Dominions validos: ");
+                System.out.println(
+                        "-gmail \n"
+                        + "-Outlook \n"
+                        + "-Yahoo \n"
+                        + "-iCloud \n"
+                        + "-PortonMail \n"
+                        + "-FastMail \n");
+            }
+        }
+        return correo;
+    }
+
+    public static String obtenerDom(String correo) {
+        String dom = "";
+        String domArr[] = correo.split("@");
+        dom = domArr[2];
+        return dom;
+    }
+
+}
---
+++ Lab1P2_EduardoCastellon/src/lab1p2_eduardocastellon/Usuario.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab1p2_eduardocastellon;
+
+/**
+ *
+ * @author caste
+ */
+public class Usuario {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String correo;
+    private String contra;
+    private String dominio;
+
+    public Usuario(String nombre, String apellido, int edad, String correo, String dominio, String contra) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.correo = correo;
+        this.dominio = dominio;
+        this.contra = contra;
+    }
+
+    public Usuario() {
+    }
+
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDominio() {
+        return dominio;
+    }
+
+    public void setDominio(String dominio) {
+        this.dominio = dominio;
+    }
+
+    public String getContra() {
+        return contra;
+    }
+
+    public void setContra(String contra) {
+        this.contra = contra;
+    }
+    
+    public String censurarContra(String contra){
+        
+        return contraC
+    }
+    
+}
