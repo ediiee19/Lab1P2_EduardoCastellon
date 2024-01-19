0b5e4f63e19320adc3302a2afd27caa394e5674d
ediiee19 <castelloneduardo2005@gmail.com>
2024-01-19 16:33:22 -0600

    avance 3

--- Lab1P2_EduardoCastellon/src/lab1p2_eduardocastellon/Lab1P2_EduardoCastellon.java
+++ Lab1P2_EduardoCastellon/src/lab1p2_eduardocastellon/Lab1P2_EduardoCastellon.java
@@ -5,11 +5,11 @@
 package lab1p2_eduardocastellon;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import javax.swing.JOptionPane;
-
 
 /**
  *
@@ -17,14 +17,11 @@ import javax.swing.JOptionPane;
  */
 public class Lab1P2_EduardoCastellon {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static ArrayList<Usuario> usuarios = new ArrayList();
+
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         Scanner lea = new Scanner(System.in);
-        
-        ArrayList<Usuario> usuarios = new ArrayList();
 
         boolean seguir = true;
         while (seguir) {
@@ -48,12 +45,14 @@ public class Lab1P2_EduardoCastellon {
                     System.out.println("Apellido");
                     String apellido = lea.nextLine();
                     String contra = validarContra();
+                    System.out.println(contra);
                     String correo = validarCorreo();
                     String dom = obtenerDom(correo);
+
                     break;
                 }
-                case 2:{
-                    
+                case 2: {
+
                     break;
                 }
             }
@@ -63,17 +62,33 @@ public class Lab1P2_EduardoCastellon {
 
     public static String validarContra() {
         Scanner lea = new Scanner(System.in);
-        
-        Pattern simbolos = Pattern.compile("^[-.*_.*&.*$.*%]$");
-        
+        String contra = "";
+
         boolean validarContra = true;
-        while(validarContra){
-            
+        while (validarContra) {
+            System.out.println("Ingrese una contraseña, \n"
+                    + "debe tener almenos uno de estos caracteres (“!“, “?”,  “<”, “>”, “$” y “%”): ");
+            contra = lea.nextLine();
+
+            if (contra.length() >= 8) {
+                boolean contraPerm = regexContra(contra);
+                if (contraPerm) {
+                    validarContra = false;
+                } else {
+                    System.out.println("Ha ingresado incorrectamente la contrase");
+                }
+            } else {
+                System.out.println("debe tener almenos 8 carcateres");
+            }
         }
-
-        
-
         return contra;
+    }
+
+    public static boolean regexContra(String contra) {
+        String regex = "^[a-zA-Z0-9._%&$+-?<>!]{8,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(contra);
+        return matcher.matches();
     }
 
     public static String validarCorreo() {
@@ -85,37 +100,67 @@ public class Lab1P2_EduardoCastellon {
             System.out.println("Ingrese su correo:");
             correo = lea.nextLine();
 
-            String dom = obtenerDom(correo);
-            if (dom.equals("gmail.com")) {
-                validarCorreo = false;
+            boolean leerCorreos = buscarCorreo(correo);
+            if (leerCorreos) {
+                boolean verificarCorr = regexCorre(correo);
+                if (verificarCorr) {
+                    String dom = obtenerDom(correo);
+                    if (dom.equals("gmail.com")) {
+                        validarCorreo = false;
+                    } else if (dom.equals("Outlook.com")) {
+                        validarCorreo = false;
+                    } else if (dom.equals("Yahoo.com")) {
+                        validarCorreo = false;
+                    } else if (dom.equals("iCloud.com")) {
+                        validarCorreo = false;
+                    } else if (dom.equals("ProtonMail.com")) {
+                        validarCorreo = false;
+                    } else if (dom.equals("FastMail.com")) {
+                        validarCorreo = false;
+                    } else {
+                        System.out.println("Ingrese un dominio valido");
+                        System.out.println("Dominions validos: ");
+                        System.out.println(
+                                "-gmail \n"
+                                + "-Outlook \n"
+                                + "-Yahoo \n"
+                                + "-iCloud \n"
+                                + "-PortonMail \n"
+                                + "-FastMail \n");
+                    }
+                } else {
+                    System.out.println("Tiene algun caracter no permitido");
+                }
+            } else {
+                System.out.println("Ya hay un correo en existencia");
             }
-            if (dom.equals("Outlook.com")) {
-                validarCorreo = false;
-            }
-            if (dom.equals("Yahoo.com")) {
-                validarCorreo = false;
-            }
-            if (dom.equals("iCloud.com")) {
-                validarCorreo = false;
-            }
-            if (dom.equals("ProtonMail.com")) {
-                validarCorreo = false;
-            }
-            if (dom.equals("FastMail.com")) {
-                validarCorreo = false;
-            }else{
-                System.out.println("Ingrese un dominio valido");
-                System.out.println("Dominions validos: ");
-                System.out.println(
-                        "-gmail \n"
-                        + "-Outlook \n"
-                        + "-Yahoo \n"
-                        + "-iCloud \n"
-                        + "-PortonMail \n"
-                        + "-FastMail \n");
-            }
+
         }
         return correo;
+    }
+
+    public static boolean buscarCorreo(String correo) {
+        boolean correoEncon = false;
+        String correoArr[] = correo.split("@");
+        for (Usuario persona : usuarios) {
+            if (correoArr[1].equals(persona.getDominio())) {
+                if (correo.equals(persona.getCorreo())) {
+                    correoEncon = true;
+                }
+            }
+        }
+        if (correoEncon) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static boolean regexCorre(String correo) {
+        String regex = "^[a-zA-Z0-9._%&$+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(correo);
+        return matcher.matches();
     }
 
     public static String obtenerDom(String correo) {
